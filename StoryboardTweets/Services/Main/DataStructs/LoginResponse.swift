e5ac05e057f099d899bc8de5b4bf632aaa422bfc
Alejandro Reyna <aleejandroreyna@gmail.com>
2023-03-14 17:53:35 -0600

    api-structs: Added login request and response

---
+++ StoryboardTweets/Services/Main/DataStructs/LoginResponse.swift
@@ -0,0 +1,11 @@
+//
+//  LoginResponse.swift
+//  StoryboardTweets
+//
+//  Created by Alejandro Reyna on 13/03/23.
+//
+
+struct LoginResponse : Codable {
+    let user : User
+    let token : String
+}
