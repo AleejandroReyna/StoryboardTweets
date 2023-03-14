530e6bda30126ee6ad3ef0ac6b98c8b0be73b093
Alejandro Reyna <aleejandroreyna@gmail.com>
2023-03-14 17:53:20 -0600

    api-structs: Added signup request and response

---
+++ StoryboardTweets/Services/Main/DataStructs/SignupRequest.swift
@@ -0,0 +1,12 @@
+//
+//  SingupRequest.swift
+//  StoryboardTweets
+//
+//  Created by Alejandro Reyna on 13/03/23.
+//
+
+struct SignupRequest : Codable {
+    let email : String
+    let names : String
+    let password : String
+}
---
+++ StoryboardTweets/Services/Main/DataStructs/SignupResponse.swift
@@ -0,0 +1,11 @@
+//
+//  SignupResponse.swift
+//  StoryboardTweets
+//
+//  Created by Alejandro Reyna on 13/03/23.
+//
+
+struct SignupResponse : Codable {
+    let user : User
+    let token : String
+}
