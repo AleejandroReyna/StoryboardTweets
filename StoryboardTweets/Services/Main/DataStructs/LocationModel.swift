cc785fa48564ab7dff840fc04d778ed07a24cc1f
Alejandro Reyna <aleejandroreyna@gmail.com>
2023-03-14 17:52:55 -0600

    api-structs: Added main models

---
+++ StoryboardTweets/Services/Main/DataStructs/LocationModel.swift
@@ -0,0 +1,11 @@
+//
+//  LocationModel.swift
+//  StoryboardTweets
+//
+//  Created by Alejandro Reyna on 13/03/23.
+//
+
+struct Location : Codable {
+    let latitude : Float
+    let longitude : Float
+}
---
+++ StoryboardTweets/Services/Main/DataStructs/TweetModel.swift
@@ -0,0 +1,19 @@
+//
+//  TweetModel.swift
+//  StoryboardTweets
+//
+//  Created by Alejandro Reyna on 13/03/23.
+//
+
+struct Tweet : Codable {
+    let id : String
+    let author : User
+    let text : String
+    let imageUrl : String?
+    let videoUrl : String?
+    let location : Location?
+    let hasVideo : Bool
+    let hasImage : Bool
+    let hasLocation : Bool
+    let createdAt : String
+}
